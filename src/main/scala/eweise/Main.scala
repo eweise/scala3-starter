3edcbc9b76e8f862580403c6b9689a1734f619df
Eric Weise <eric.weise@chime.com>
2021-10-13 22:28:11 -0700

    add exceptions

--- src/main/scala/eweise/Main.scala
+++ src/main/scala/eweise/Main.scala
@@ -12,14 +12,22 @@ object MinimalApplication extends cask.MainRoutes {
 
   todoRepo.initialize()
 
-//  implicit val foo: java.time.LocalDate = upickle.default.macroRW[java.time.LocalDate]
-//  upickle.default.macroRW[Todo]
+  //  implicit val foo: java.time.LocalDate = upickle.default.macroRW[java.time.LocalDate]
+  //  upickle.default.macroRW[Todo]
 
   @cask.get("/")
   def hello() = "Hello World!"
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) = request.text().reverse
+  @cask.post("/todo")
+  def doThing(request: cask.Request) = {
+    val one = "world"
+    val text = request.text()
+    val json = ujson.read(text)
+    val todo = Todo(
+      title = json("title").str,
+      description = Some(json("description").str)
+    )
+  }
 
   override def port: Int = 8082
 
